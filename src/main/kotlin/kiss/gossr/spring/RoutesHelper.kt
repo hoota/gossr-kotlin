c44b5c6d0bb72023e76292bd75553e47002dcbbb
Andrey Stroganov <stroganoff@gmail.com>
2026-07-07 14:50:45 +0700

    Allow nullable bean/method in addRoute, bump version to 1.0.1-spring-boot-3

--- src/main/kotlin/kiss/gossr/spring/RoutesHelper.kt
+++ src/main/kotlin/kiss/gossr/spring/RoutesHelper.kt
@@ -158,7 +158,7 @@ class RoutesHelper(
         }
     }
 
-    fun <T : Route> addRoute(bean: Any, method: Method, type: Class<T>) {
+    fun <T : Route> addRoute(bean: Any?, method: Method?, type: Class<T>) {
         val requestMethods = listOfNotNull(
             if(GetRoute::class.java.isAssignableFrom(type)) RequestMethod.GET else null,
             if(PostRoute::class.java.isAssignableFrom(type)) RequestMethod.POST else null,
@@ -202,8 +202,8 @@ class RoutesHelper(
                 } ?: emptyList(),
             bean = bean,
             method = method,
-            annotations = method.annotations ?: emptyArray(),
-            conditions = getConditions(bean, type)
+            annotations = method?.annotations ?: emptyArray(),
+            conditions = bean?.let { getConditions(it, type) }
         )
     }
 
